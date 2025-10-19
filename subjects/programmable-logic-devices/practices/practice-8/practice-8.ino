c989f2842e989e46d0302cafa9a2052f94a206c1
fermeridamagni <hello@magni.dev>
2025-10-18 21:17:24 -0600

    Add new practice files for practices 5, 7, 8, and 9

---
+++ subjects/programmable-logic-devices/practices/practice-8/practice-8.ino
@@ -0,0 +1,26 @@
+const int sensorPin = A0;
+const int ledPin = 7;
+
+void setup() {
+  Serial.begin(9600);
+
+  pinMode(ledPin, OUTPUT);
+  pinMode(sensorPin, INPUT);
+}
+
+
+void loop() {
+  int reading = analogRead(sensorPin);
+
+  if (reading < 100) {
+    digitalWrite(ledPin, HIGH);
+  } else {
+    digitalWrite(ledPin, LOW);
+  }
+   
+  Serial.print("Reading: ");
+  Serial.print(reading);
+  Serial.println();
+
+  delay(200);
+}
